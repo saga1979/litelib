3c8ee6a41ef6165daee599eee05719ac9777115d
saga1979 <deprovedangel@163.com>
2018-06-14 18:11:19 +0800

    Update lite_lib.h

--- lite_lib.h
+++ lite_lib.h
@@ -3,14 +3,10 @@
 
 #define  LITE_LIB_VER "0.1000000000"
 
-#include "lite_dir.h"
-#include "lite_file.h"
+
 #include "lite_hash.h"
-#include "lite_keyfile.h"
+
 #include "lite_list.h"
-#include "lite_log.h"
-#include "lite_stream.h"
-#include "lite_string.h"
-#include "lite_timer.h"
+
 
 #endif
